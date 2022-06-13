fd314597b4e3f0aa401c75d960c15cde1c56e0fb
s0566454 <s0566454@htw-berlin.de>
2022-06-13 22:10:46 +0200

    components added

---
+++ Software/myweather/src/services/TempratueAndDetails.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import{
+    UilArrowup,
+    UilArrowDown,
+    UilTemperature,
+    UilTear,
+    UilWind,
+    UilSun,
+    UilSunset,
+} from "@iconscout/react-unicons";
+
+
+
+function TempratueAndDetails({
+    weather: {
+      details,
+      icon,
+      temp,
+      temp_min,
+      temp_max,
+      sunrise,
+      sunset,
+      speed,
+      humidity,
+      feels_like,
+      timezone,
+    },
+  }) {
+  return (
+    <div>
+      
+     
+    </div>
+  )
+}
+
+export default TempratueAndDetails
